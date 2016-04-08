1aa45f261509a0bd6e48f7536e09902786023467
Noam Bergauz <noam@spectory.com>
2016-04-08 12:34:36 +0300

    yt api search with state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/searchbar';
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyAwaMoZoMigJWZR-gSNoqW1Gedzfx6rXE0';
 
-const App = () => {
-	return <div> Hi! </div>;
-}; 
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surf'}, (data) => {
+			this.setState({videos: data});
+		});
+
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}	
+}
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
